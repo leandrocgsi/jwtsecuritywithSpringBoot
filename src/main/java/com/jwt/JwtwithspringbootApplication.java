9c97e5ba59a04403f53909b7d12c6b711772e1f0
tonmoysaha <tonmoysahaopi@gmail.com>
2020-05-03 08:49:29 +0600

    initial commit

---
+++ src/main/java/com/jwt/JwtwithspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtwithspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtwithspringbootApplication.class, args);
+	}
+
+}
