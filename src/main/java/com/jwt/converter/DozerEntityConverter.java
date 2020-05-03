08e1c7e5d8cae8ee998b2901b21443a0303f33ba
tonmoysaha <tonmoysahaopi@gmail.com>
2020-05-03 20:06:08 +0600

    adding dozerconverter

--- src/main/java/com/jwt/converter/DozerEntityConverter.java
+++ src/main/java/com/jwt/converter/DozerEntityConverter.java
@@ -1,5 +1,25 @@
 package com.jwt.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
 public class DozerEntityConverter {
 
+	private static final Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	
+	public static <O, D> D parseObject(O origin , Class<D> destination){
+		return mapper.map(origin, destination);
+	}
+	
+	public static <O, D> List<D> parseListObject(List<O> origin , Class<D> destination){
+		List<D> dastinationObjects = new ArrayList<D>();
+		
+		for (Object o : origin) {
+			dastinationObjects.add(mapper.map(o, destination));
+		}
+		return dastinationObjects;
+	}
 }
