84b7071c8792b4d9820192ad143c76a10cadf319
tonmoysaha <tonmoysahaopi@gmail.com>
2020-05-04 13:44:39 +0600

    change content by param

--- src/main/java/com/jwt/config/WebDataConfig.java
+++ src/main/java/com/jwt/config/WebDataConfig.java
@@ -12,8 +12,18 @@ public class WebDataConfig implements WebMvcConfigurer{
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorParameter(false)
+		//http://localhost:8080/person/1.xml
+//		configurer.favorParameter(false)
+//		.ignoreAcceptHeader(false)
+//		.defaultContentType(MediaType.APPLICATION_JSON)
+//		.mediaType("json", MediaType.APPLICATION_JSON)
+//		.mediaType("xml", MediaType.APPLICATION_XML);
+		
+		configurer.favorPathExtension(false)
+		.favorParameter(true)
 		.ignoreAcceptHeader(false)
+		.parameterName("mediatype")
+		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("json", MediaType.APPLICATION_JSON)
 		.mediaType("xml", MediaType.APPLICATION_XML);
