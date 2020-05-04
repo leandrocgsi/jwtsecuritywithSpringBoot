5fdbad61e66ae1b0eca45c06209789f3cae25d89
tonmoysaha <tonmoysahaopi@gmail.com>
2020-05-04 10:58:40 +0600

    migrating database using flywaydb

--- src/main/java/com/jwt/controller/PersonController.java
+++ src/main/java/com/jwt/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,8 +51,9 @@ public class PersonController {
 	
 	@RequestMapping(value="/{id}", 
 			method=RequestMethod.DELETE)
-	public void delete(@PathVariable("id") Long id) {
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
 		service.delete(id);
+		return ResponseEntity.ok().build();
 	}	
 	
 }
